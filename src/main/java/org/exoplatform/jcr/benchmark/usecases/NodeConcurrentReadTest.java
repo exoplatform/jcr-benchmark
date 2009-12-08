c9102c829d69f04cb37f91f51d84066a8cf042eb
Dmytro Katayev <dmytro.katayev@exoplatform.com>
2009-12-08 14:08:36 +0000

    EXOJCR-258 NodeConcurrentReadTest changed

--- src/main/java/org/exoplatform/jcr/benchmark/usecases/NodeConcurrentReadTest.java
+++ src/main/java/org/exoplatform/jcr/benchmark/usecases/NodeConcurrentReadTest.java
@@ -1,20 +1,18 @@
 /*
- * Copyright (C) 2009 eXo Platform SAS.
+ * Copyright (C) 2003-2009 eXo Platform SAS.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
  */
 package org.exoplatform.jcr.benchmark.usecases;
 
@@ -23,95 +21,90 @@ import com.sun.japex.TestCase;
 import org.exoplatform.jcr.benchmark.JCRTestBase;
 import org.exoplatform.jcr.benchmark.JCRTestContext;
 
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 import javax.jcr.Node;
 
 /**
  * @author <a href="mailto:dmitry.kataev@exoplatform.com">Dmytro Katayev</a>
  * @version $Id$
- *
  */
-public class NodeConcurrentReadTest extends JCRTestBase
-{
+public class NodeConcurrentReadTest extends JCRTestBase {
 
-   private Node root;
+  private Node           rootNode    = null;
 
-   private Node node;
+  private List<String>   parentNames = Collections.synchronizedList(new ArrayList<String>());
 
-   private int iterations = 100;
-   
-   private Writer writer;
+  private JCRTestContext context     = null;
 
-   @Override
-   public void doFinish(TestCase tc, JCRTestContext context) throws Exception
-   {
-      super.doFinish(tc, context);
+  private volatile int   iterations  = 0;
 
-      writer.destroy();
-      node.remove();
+  private int            threads     = 0;
+
+  /**
+   * @see org.exoplatform.jcr.benchmark.JCRTestBase#doPrepare(com.sun.japex.TestCase,
+   *      org.exoplatform.jcr.benchmark.JCRTestContext)
+   */
+  @Override
+  public void doPrepare(TestCase tc, JCRTestContext context) throws Exception {
+    super.doPrepare(tc, context);
+
+    this.context = context;
+
+    iterations = tc.getIntParam("japex.runIterations");
+    iterations = tc.getIntParam("japex.numberOfThreads");
+
+    rootNode = context.getSession().getRootNode().addNode(context.generateUniqueName("testRoot"));
+
+    for (int i = 0; i < iterations; i++) {
+      String parentName = context.generateUniqueName("parent");
+      Node parent = rootNode.addNode(parentName);
+      parent.setProperty("testProp", "testVal");
       context.getSession().save();
-   }
+      parentNames.add(parentName);
+    }
 
-   @Override
-   public void doPrepare(TestCase tc, JCRTestContext context) throws Exception
-   {
-      super.doPrepare(tc, context);
+    for (int i = 0; i < threads; i++) {
+      new Writer().start();
+    }
 
-      root = context.getSession().getRootNode().addNode(context.generateUniqueName("testRoot"));
-      node = root.addNode(context.generateUniqueName("testConcurentNode"));
-      
-      writer = new Writer();
-      writer.start();
+  }
 
-   }
+  /**
+   * @see org.exoplatform.jcr.benchmark.JCRTestBase#doRun(com.sun.japex.TestCase,
+   *      org.exoplatform.jcr.benchmark.JCRTestContext)
+   */
+  @Override
+  public void doRun(TestCase tc, JCRTestContext context) throws Exception {
+    readProp();
+  }
 
-   /**
-    * @see org.exoplatform.jcr.benchmark.JCRTestBase#doRun(com.sun.japex.TestCase, org.exoplatform.jcr.benchmark.JCRTestContext)
-    */
-   @Override
-   public void doRun(TestCase tc, JCRTestContext context) throws Exception
-   {
+  private class Writer extends Thread {
 
-      try
-      {
-         for (int i = 0; i < iterations; i++)
-         {
-            node.getProperty("name");
-            node.getProperty("date");
-            node.getProperty("time");
-            node.getProperties();
-         }
-
+    /**
+     * @see java.lang.Thread#run()
+     */
+    @Override
+    public void run() {
+      super.run();
+      try {
+        String name = context.generateUniqueName(this.getName());
+        Node writeNode = rootNode.addNode(name);
+        writeNode.setProperty("testProp", this.getName());
+        context.getSession().save();
+        iterations++;
+      } catch (Exception e) {
+        e.printStackTrace();
       }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-      }
+    }
+  }
 
-   }
+  private void readProp() throws Exception {
+    Node readNode = rootNode.getNode(parentNames.get(new Random().nextInt(iterations)));
+    readNode.getProperty("testProp").getValue().getString();
+  }
 
-   class Writer extends Thread
-   {
-
-      public void run()
-      {
-         try
-         {
-            for (int i = 0; i < iterations; i++)
-            {
-               node.setProperty("name", this.getName());
-               node.setProperty("date", Calendar.getInstance());
-               node.setProperty("time", System.currentTimeMillis());
-               this.wait(100);
-            }
-         }
-         catch (Exception e)
-         {
-            e.printStackTrace();
-         }
-
-      }
-   }
- 
 }
