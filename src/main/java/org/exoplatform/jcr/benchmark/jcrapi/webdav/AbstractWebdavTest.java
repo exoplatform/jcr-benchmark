f79fdfe05d899b5d88f364f9395fee0f47fd096f
Dmytro Katayev <dmytro.katayev@exoplatform.com>
2009-12-09 13:31:31 +0000

    EXOJCR-286 https://jira.jboss.org/jira/browse/EXOJCR-286 org.exoplatform.jcr.benchmark.jcrapi.webdav package added

---
+++ src/main/java/org/exoplatform/jcr/benchmark/jcrapi/webdav/AbstractWebdavTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.jcr.benchmark.jcrapi.webdav;
+
+import com.sun.japex.TestCase;
+
+import org.exoplatform.common.http.client.CookieModule;
+import org.exoplatform.jcr.benchmark.JCRTestBase;
+import org.exoplatform.jcr.benchmark.JCRTestContext;
+
+/**
+ * @author <a href="mailto:dmitry.kataev@exoplatform.com">Dmytro Katayev</a>
+ * @version $Id$
+ *
+ */
+public abstract class AbstractWebdavTest extends JCRTestBase
+{
+
+   protected JCRWebdavConnection item;
+
+   /**
+    * @see org.exoplatform.jcr.benchmark.JCRTestBase#doPrepare(com.sun.japex.TestCase, org.exoplatform.jcr.benchmark.JCRTestContext)
+    */
+   public void doPrepare(TestCase tc, JCRTestContext context) throws Exception
+   {
+      super.doPrepare(tc, context);
+      CookieModule.setCookiePolicyHandler(null);
+      item = new JCRWebdavConnection(tc);
+   }
+
+   /**
+    * @see org.exoplatform.jcr.benchmark.JCRTestBase#doFinish(com.sun.japex.TestCase, org.exoplatform.jcr.benchmark.JCRTestContext)
+    */
+   @Override
+   public void doFinish(TestCase tc, JCRTestContext context) throws Exception
+   {
+
+      item.stop();
+      super.doFinish(tc, context);
+   }
+
+}
---
+++ src/main/java/org/exoplatform/jcr/benchmark/jcrapi/webdav/JCRWebdavConnection.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.jcr.benchmark.jcrapi.webdav;
+
+import com.sun.japex.TestCase;
+
+import org.exoplatform.common.http.client.HTTPConnection;
+import org.exoplatform.common.http.client.ModuleException;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="mailto:dmitry.kataev@exoplatform.com">Dmytro Katayev</a>
+ * @version $Id$
+ *
+ */
+public class JCRWebdavConnection extends HTTPConnection
+{
+
+   private String realm;
+
+   private String user;
+
+   private String pass;
+
+   private String workspacePath;
+
+   public JCRWebdavConnection(TestCase tc)
+   {
+      super(tc.getParam("webdav.host"), tc.getIntParam("webdav.port"));
+
+      user = tc.getParam("webdav.user");
+      pass = tc.getParam("webdav.pass");
+      realm = tc.getParam("webdav.realm");
+
+      addBasicAuthorization(realm, user, pass);
+
+      workspacePath = tc.getParam("webdav.workspacePath");
+   }
+
+   public void addNode(String name) throws IOException, ModuleException
+   {
+      Put(workspacePath + name, "");
+   }
+
+   public void removeNode(String name) throws IOException, ModuleException
+   {
+      Delete(workspacePath + name);
+   }
+
+}
