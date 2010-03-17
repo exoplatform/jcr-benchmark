36cb2f1d97f1c8e616090763fcc5771acb615a74
Nikolay  Zamosenchuk <nikolay.zamosenchuk@exoplatform.com>
2010-03-17 06:56:15 +0000

    EXOJCR-551: Fixed keypress waiting for Windows, that sends \r\n instead of \n. So System.in.read() can't be used (last char is saved in buffer and next keypress wait passes immediately)

--- src/main/java/org/exoplatform/jcr/benchmark/usecases/portal/PageUsecasesTest.java
+++ src/main/java/org/exoplatform/jcr/benchmark/usecases/portal/PageUsecasesTest.java
@@ -27,9 +27,11 @@ import org.exoplatform.services.jcr.util.IdGenerator;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -133,7 +135,8 @@ public class PageUsecasesTest extends JCRTestBase
       if (keyPressThreadCounter >= timesToWait)
       {
          System.out.print("Press Enter to continue ...");
-         System.in.read();
+         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+         in.readLine();
          System.out.println("Continuing ...");
          keyPressThreadCounter = 0;
       }
