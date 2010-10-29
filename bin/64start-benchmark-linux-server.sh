54df611067fe5bfcfb5922b37f2eec018d2690cf
Anatoliy Bazko <abazko@exoplatform.com>
2010-10-29 09:44:50 +0000

    JCR-1493: fix sh-files

--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,3 +1,3 @@
-/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.12.6-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
-/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.12.6-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-#/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.12.6-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
+/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.12.5.jar ../config/JCRAPI.xml -last -line
+/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.12.5.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+#/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.12.5.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
