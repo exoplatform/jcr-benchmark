49791a30aa07673e374b2779ff8d88ce83ed5823
Anatoliy Bazko <abazko@exoplatform.com>
2010-12-13 13:13:12 +0000

    JCR-1546: change dep to 1.12.7-GA-SNAPSHOT

--- bin/start-benchmark.cmd
+++ bin/start-benchmark.cmd
@@ -1,3 +1,3 @@
-java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.12.6-SNAPSHOT.jar ../config/JCRAPI.xml -last
-java -cp ../lib/exo-jcr-benchmark-1.12.6-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-rem java -cp ../lib/exo-jcr-benchmark-1.12.6-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.12.7-SNAPSHOT.jar ../config/JCRAPI.xml -last
+java -cp ../lib/exo-jcr-benchmark-1.12.7-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+rem java -cp ../lib/exo-jcr-benchmark-1.12.7-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
