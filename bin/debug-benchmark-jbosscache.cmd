befbb414275d17204dac68709f1cae843b86d8d5
Anatoliy Bazko <abazko@exoplatform.com>
2011-04-29 09:16:50 +0000

    EXOJCR-1299: fix version

--- bin/debug-benchmark-jbosscache.cmd
+++ bin/debug-benchmark-jbosscache.cmd
@@ -1,7 +1,7 @@
 copy /y ..\config\test-configuration-benchmark-jbc.xml ..\config\test-configuration-benchmark.xml
 copy /y ..\config\test-jcr-config-benchmark-jbc.xml ..\config\test-jcr-config-benchmark.xml
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Duser.region=us %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.14.0-CR1-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.14.0-CR1-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Duser.region=us %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
 
-@rem java -cp ../lib/exo-jcr-benchmark-1.14.0-CR1-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+@rem java -cp ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
