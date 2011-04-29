b320e6b48e015e88a038fda716ce563361c9c4bc
Anatoliy Bazko <abazko@exoplatform.com>
2011-04-29 09:21:22 +0000

    EXOJCR-1299: fix version

--- bin/64start-benchmark-linux-server-jbosscache.sh
+++ bin/64start-benchmark-linux-server-jbosscache.sh
@@ -2,4 +2,4 @@ cp -f ../config/test-configuration-benchmark-jbc.xml ../config/test-configuratio
 cp -f ../config/test-jcr-config-benchmark-jbc.xml ../config/test-jcr-config-benchmark.xml
 /usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
 /usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-#/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.0-gA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
+#/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
