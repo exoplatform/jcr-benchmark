a9dbd2167618a5821f6621dca3518841933b469a
Anatoliy Bazko <abazko@exoplatform.com>
2011-09-05 15:11:02 +0000

    EXOJCR-1489: set new version 1.14.1-GA-SNAPSHOT

--- bin/start-benchmark.cmd
+++ bin/start-benchmark.cmd
@@ -1,5 +1,5 @@
 copy /y ..\config\test-configuration-benchmark-jbc.xml ..\config\test-configuration-benchmark.xml
 copy /y ..\config\test-jcr-config-benchmark-jbc.xml ..\config\test-jcr-config-benchmark.xml
-java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-java -cp ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-rem java -cp ../lib/exo-jcr-benchmark-1.14.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.1-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+java -cp ../lib/exo-jcr-benchmark-1.14.1-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+rem java -cp ../lib/exo-jcr-benchmark-1.14.1-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
