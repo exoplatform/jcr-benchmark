376c570108c584ebda743a96275c0dcf13db9ccc
Anatoliy Bazko <abazko@exoplatform.com>
2011-10-17 13:16:06 +0000

    EXOJCR-1514: change version to 1.14.3-GA-SNAPSHOT

--- bin/64start-debug.sh
+++ bin/64start-debug.sh
@@ -1,6 +1,6 @@
 cp -f ../config/test-configuration-benchmark-ispn.xml ../config/test-configuration-benchmark.xml
 cp -f ../config/test-jcr-config-benchmark-ispn.xml ../config/test-jcr-config-benchmark.xml
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044  -Xmx1500m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.2-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-#/usr/lib/java64/jre/bin/java -Xmx15000m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.2-GA-SNAPSHOT.jar ../config/JCRAPI-usecases.xml -last
-#/usr/lib/java64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.2-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-#/usr/lib/java64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.2-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044  -Xmx1500m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.3-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+#/usr/lib/java64/jre/bin/java -Xmx15000m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.3-GA-SNAPSHOT.jar ../config/JCRAPI-usecases.xml -last
+#/usr/lib/java64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.3-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+#/usr/lib/java64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.3-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
