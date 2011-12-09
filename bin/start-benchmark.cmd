5e10d7ffbad297814b5339ea6f5fafb15dc801a1
Anatoliy Bazko <abazko@exoplatform.com>
2011-12-09 12:53:49 +0000

    EXOJCR-1674: fix cmd-files

--- bin/start-benchmark.cmd
+++ bin/start-benchmark.cmd
@@ -1,5 +1,5 @@
-copy /y ..\config\test-configuration-benchmark-jbc.xml ..\config\test-configuration-benchmark.xml
-copy /y ..\config\test-jcr-config-benchmark-jbc.xml ..\config\test-jcr-config-benchmark.xml
+copy /y ..\config\test-configuration-benchmark-ispn.xml ..\config\test-configuration-benchmark.xml
+copy /y ..\config\test-jcr-config-benchmark-ispn.xml ..\config\test-jcr-config-benchmark.xml
 java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.5-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
 java -cp ../lib/exo-jcr-benchmark-1.14.5-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
 rem java -cp ../lib/exo-jcr-benchmark-1.14.5-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
