357c57b08bc733201b35e6f9b8274f5be159ca0f
Anatoliy Bazko <abazko@exoplatform.com>
2011-12-30 09:24:16 +0000

    EXOJCR-1595: changed deps to branch

--- bin/start-benchmark-jbosscache.cmd
+++ bin/start-benchmark-jbosscache.cmd
@@ -1,5 +1,4 @@
 copy /y ..\config\test-configuration-benchmark-jbc.xml ..\config\test-configuration-benchmark.xml
 copy /y ..\config\test-jcr-config-benchmark-jbc.xml ..\config\test-jcr-config-benchmark.xml
-java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.6-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-java -cp ../lib/exo-jcr-benchmark-1.14.6-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-rem java -cp ../lib/exo-jcr-benchmark-1.14.6-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last
+java -cp ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
