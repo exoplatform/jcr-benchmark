f077c7b48ce6c9ae0a0690ebcc8d4e416a6ad5a0
Anatoliy Bazko <abazko@exoplatform.com>
2012-01-20 10:43:56 +0000

    EXOJCR-821: allow to use closed sessions in tests

--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,4 +1,4 @@
 cp -f ../config/test-configuration-benchmark-ispn.xml ../config/test-configuration-benchmark.xml
 cp -f ../config/test-jcr-config-benchmark-ispn.xml ../config/test-jcr-config-benchmark.xml
-/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
 /usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
--- bin/64start-debug-jbosscache.sh
+++ bin/64start-debug-jbosscache.sh
@@ -1,3 +1,3 @@
 cp -f ../config/test-configuration-benchmark-jbc.xml ../config/test-configuration-benchmark.xml
 cp -f ../config/test-jcr-config-benchmark-jbc.xml ../config/test-jcr-config-benchmark.xml
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044  -Xmx1500m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044  -Xmx1500m -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last
