8b04be1b8117ddc9585dfbab8874a041757317ab
Anatoliy Bazko <abazko@exoplatform.com>
2012-03-19 11:49:26 +0000

    JCR-1719: change version to 1.12.13-GA-SNAPSHOT

--- bin/start-benchmark.cmd
+++ bin/start-benchmark.cmd
@@ -1,3 +1,3 @@
-java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.12.12-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-java -cp ../lib/exo-jcr-benchmark-1.12.12-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-rem java -cp ../lib/exo-jcr-benchmark-1.12.12-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+java -Xmx800m -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.12.13-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+java -cp ../lib/exo-jcr-benchmark-1.12.13-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+rem java -cp ../lib/exo-jcr-benchmark-1.12.13-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
