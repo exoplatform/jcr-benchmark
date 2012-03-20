ef9f99cd1441d5c29af0ebd3f2953856421bd37d
Anatoliy Bazko <abazko@exoplatform.com>
2012-03-20 06:54:20 +0000

    EXOJCR-1676: changed version to 1.14.8-GA-SHAPSHOT

--- bin/debug-benchmark.cmd
+++ bin/debug-benchmark.cmd
@@ -1,7 +1,7 @@
 copy /y ..\config\test-configuration-benchmark-ispn.xml ..\config\test-configuration-benchmark.xml
 copy /y ..\config\test-jcr-config-benchmark-ispn.xml ..\config\test-jcr-config-benchmark.xml
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Duser.region=us %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.14.7-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.14.7-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Duser.region=us %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.14.8-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.14.8-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
 
-@rem java -cp ../lib/exo-jcr-benchmark-1.14.7-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+@rem java -cp ../lib/exo-jcr-benchmark-1.14.8-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
