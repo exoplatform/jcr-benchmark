ecf3c16e4176f7db6c8f999762d281d7550485e9
Anatoliy Bazko <abazko@exoplatform.com>
2012-06-11 15:32:08 +0300

    JCR-1773: update eXo JCR version to 1.12.14-GA-SNAPSHOT

--- bin/debug-benchmark.cmd
+++ bin/debug-benchmark.cmd
@@ -1,5 +1,5 @@
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Duser.region=us %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.12.13-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.12.13-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Duser.region=us %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.12.14-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.12.14-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
 
-@rem java -cp ../lib/exo-jcr-benchmark-1.12.13-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
+@rem java -cp ../lib/exo-jcr-benchmark-1.12.14-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.AddNtFileWithMetadataNoJapex
