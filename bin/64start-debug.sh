c878191af93c2e3fc2a0f083aec686ba4e70d7e4
Anatoliy Bazko <abazko@exoplatform.com>
2012-07-31 10:13:34 +0300

    JCR-1838: cleanup benchmark conf

--- bin/64start-debug.sh
+++ bin/64start-debug.sh
@@ -1,3 +1,2 @@
-cp -f ../config/test-configuration-benchmark-ispn.xml ../config/test-configuration-benchmark.xml
 cp -f ../config/test-jcr-config-benchmark-ispn.xml ../config/test-jcr-config-benchmark.xml
 java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044  -Xmx1500m -Duser.language=en -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last
--- bin/debug-benchmark-jbosscache.cmd
+++ bin/debug-benchmark-jbosscache.cmd
@@ -1,4 +1,3 @@
-copy /y ..\config\test-configuration-benchmark-jbc.xml ..\config\test-configuration-benchmark.xml
 copy /y ..\config\test-jcr-config-benchmark-jbc.xml ..\config\test-jcr-config-benchmark.xml
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
 @java -Xmx800m -Duser.language=en -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last
