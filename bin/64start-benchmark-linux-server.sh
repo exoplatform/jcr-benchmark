c8a55cb91efd5a3771e32513c7c8293eb30f07ac
Anatoliy Bazko <abazko@exoplatform.com>
2012-09-11 09:35:14 +0300

    JCR-1785: release 1.15.0-Beta01

--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,2 +1,2 @@
-/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
-/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
--- bin/debug-benchmark.cmd
+++ bin/debug-benchmark.cmd
@@ -1,3 +1,3 @@
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.15.0-Beta01-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
