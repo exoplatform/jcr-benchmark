d32588ec62f497a3404453af40d97ba5fd468d9a
Dmitry Kuleshov <dkuleshov@exoplatform.com>
2012-09-27 16:54:10 +0300

    JCR-1923: fixed system property usage to correctly set IpV4 stack as preferred

--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,2 +1,2 @@
-/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+/usr/lib/java6_x64/jre/bin/java -Xmx12g -Djava.net.preferIPv4Stack=true -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
 /usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.15.0-CR1-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
