0b9392b4018eea1f63952351ef8c8a200e0c19d1
Anatoliy Bazko <abazko@exoplatform.com>
2012-10-24 15:35:33 +0300

    JCR-1897: changed to new eXo JCR version 1.15.0-GA

--- bin/debug-benchmark.cmd
+++ bin/debug-benchmark.cmd
@@ -1,3 +1,3 @@
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Dlog4j.configuration=file:../config/log4j.properties -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.15.0-CR2-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.15.0-CR2-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Dlog4j.configuration=file:../config/log4j.properties -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.15.0-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.15.0-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
