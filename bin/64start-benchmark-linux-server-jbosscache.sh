0ae88ef19f422d3e0699eaa59ef2545647abfb7b
Anatoliy Bazko <abazko@exoplatform.com>
2012-12-03 09:33:16 +0200

    JCR-2007: update JCR version

--- bin/64start-benchmark-linux-server-jbosscache.sh
+++ bin/64start-benchmark-linux-server-jbosscache.sh
@@ -1,5 +1,5 @@
 cp -f ../config/test-configuration-benchmark-jbc.xml ../config/test-configuration-benchmark.xml
 cp -f ../config/test-jcr-config-benchmark-jbc.xml ../config/test-jcr-config-benchmark.xml
-/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.9-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
-/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.9-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
-#/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.9-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
+/usr/lib/java6_x64/jre/bin/java -Xmx12g -Duser.language=en -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.14.10-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.10-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+#/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.14.10-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.QueryNodeLoader
