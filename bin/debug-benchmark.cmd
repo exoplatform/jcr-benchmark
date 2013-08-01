5c03a1da5ba83e0855ffcc5d8b22c55de4e66666
aymen <aymen.boughzela@exoplatform.com>
2013-08-01 16:14:22 +0100

    JCR-1.16.0-Alpha1: changed dependencies to development versions

--- bin/debug-benchmark.cmd
+++ bin/debug-benchmark.cmd
@@ -1,3 +1,3 @@
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Dlog4j.configuration=file:../config/log4j.properties -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.15.2-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.15.2-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Dlog4j.configuration=file:../config/log4j.properties -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.16.0-Alpha2-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.16.0-Alpha2-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
