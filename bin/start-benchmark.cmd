fca4f6796bb85430b34e1806308d6053aa51efda
aymen <aboughzela@exoplatform.com>
2013-10-31 01:41:11 +0100

    JCR-1.16.0-Alpha3: changed dependencies to development versions

--- bin/start-benchmark.cmd
+++ bin/start-benchmark.cmd
@@ -1,2 +1,2 @@
-java -Xmx800m -Duser.language=en -Duser.region=us -Dlog4j.configuration=file:../config/log4j.properties -Dexo.jcr.allow.closed.session.usage=true -jar ../lib/exo-jcr-benchmark-1.16.0-Alpha3-SNAPSHOT.jar ../config/JCRAPI.xml -last
-java -cp ../lib/exo-jcr-benchmark-1.16.0-Alpha3-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+java -Xmx800m -Duser.language=en -Duser.region=us -Dlog4j.configuration=file:../config/log4j.properties -Dexo.jcr.allow.closed.session.usage=true -jar ../lib/exo-jcr-benchmark-1.16.0-Alpha4-SNAPSHOT.jar ../config/JCRAPI.xml -last
+java -cp ../lib/exo-jcr-benchmark-1.16.0-Alpha4-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
