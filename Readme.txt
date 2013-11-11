f1b4f104efff4e4357305cfa8f89a52d12b6438b
Nicolas Filotto <nfilotto@exoplatform.com>
2013-11-11 14:38:52 +0000

    JCR-2234: Make sure that the JDBC Cache store used by the LockManager is optimized and reliable (jcr-benchmark)

--- Readme.txt
+++ Readme.txt
@@ -2,8 +2,3 @@
 2. Go to target\exo-jcr-benchmark-binary.dir\bin and run start-benchmark.cmd.
 By default this benchmark is configured for pgsql DB.
 Make sure you have configured mysql DB as in config\test-configuration-benchmark.xml.
-
-
-The benchmark for Infinispan
-1. Run "mvn clean install -Pispn" command.
-2. Go to target\exo-jcr-benchmark-binary.dir\bin and run start-benchmark.cmd.
--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,2 +1,2 @@
-/usr/lib/java6_x64/jre/bin/java -Xmx12g -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:../config/log4j.properties -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.16.0-Alpha3-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
-/usr/lib/java6_x64/jre/bin/java -cp ../lib/exo-jcr-benchmark-1.16.0-Alpha3-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+$JAVA_HOME/bin/java -Xmx12g -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:../config/log4j.properties -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.16.0-Alpha3-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+$JAVA_HOME/bin/java -cp ../lib/exo-jcr-benchmark-1.16.0-Alpha3-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
