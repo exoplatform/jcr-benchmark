390c5bd95401e993044c91b1a4ce48262a442abb
Nicolas Filotto <nfilotto@exoplatform.com>
2014-04-10 09:06:07 +0000

    JCR-2295: changed dependencies to the next development versions

--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,2 +1,2 @@
-java -Xmx12g -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:../config/log4j.properties -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.8-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
-java -cp ../lib/exo-jcr-benchmark-1.15.8-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+java -Xmx12g -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:../config/log4j.properties -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.15.9-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+java -cp ../lib/exo-jcr-benchmark-1.15.9-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
