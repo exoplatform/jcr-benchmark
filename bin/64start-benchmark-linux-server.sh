8400b82af4f6c779c94bb9ac1a7215b5737527b9
aymen <aboughzela@exoplatform.com>
2014-07-05 12:40:36 +0100

    JCR-2313 : changed dependencies to development versions

--- bin/64start-benchmark-linux-server.sh
+++ bin/64start-benchmark-linux-server.sh
@@ -1,2 +1,2 @@
-$JAVA_HOME/bin/java -Xmx12g -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:../config/log4j.properties -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.16.1-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
-$JAVA_HOME/bin/java -cp ../lib/exo-jcr-benchmark-1.16.1-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+$JAVA_HOME/bin/java -Xmx12g -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:../config/log4j.properties -Duser.language=en -Dexo.jcr.allow.closed.session.usage=true -Duser.region=us -jar ../lib/exo-jcr-benchmark-1.16.x-SNAPSHOT.jar ../config/JCRAPI.xml -last -line
+$JAVA_HOME/bin/java -cp ../lib/exo-jcr-benchmark-1.16.x-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
--- bin/debug-benchmark.cmd
+++ bin/debug-benchmark.cmd
@@ -1,3 +1,3 @@
 @SET JPDA_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,address=18000,server=y,suspend=y
-@java -Xmx800m -Duser.language=en -Dlog4j.configuration=file:../config/log4j.properties -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.16.1-GA-SNAPSHOT.jar ../config/JCRAPI.xml -last
-@java -cp ../lib/exo-jcr-benchmark-1.16.1-GA-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
+@java -Xmx800m -Duser.language=en -Dlog4j.configuration=file:../config/log4j.properties -Duser.region=us -Dexo.jcr.allow.closed.session.usage=true %JPDA_OPTS% -jar ../lib/exo-jcr-benchmark-1.16.x-SNAPSHOT.jar ../config/JCRAPI.xml -last
+@java -cp ../lib/exo-jcr-benchmark-1.16.x-SNAPSHOT.jar org.exoplatform.jcr.benchmark.helpers.SimpleReportHelper
